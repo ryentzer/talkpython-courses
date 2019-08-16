3a2629094383e922bf110b06b065e4447dfe3615
Rick Yentzer <rick@forbenmedia.com>
2019-08-16 12:37:59 -0400

    complete app 1

--- ten-apps/01-hello-world/program.py
+++ ten-apps/01-hello-world/program.py
@@ -1,1 +1,9 @@
 # App 1 Hello World
+print('===============================')
+print('===== Simple Hello App')
+print('===============================')
+
+print('Hi, my name is Guido.')
+name = input('What is your name? ')
+
+print('Nice to meet you, {}'.format(name))
